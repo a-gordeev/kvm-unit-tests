dfc3889efa9dce56daae23e0f92cb536350c3e3f
Alexander Gordeev <agordeev@redhat.com>
2016-08-16 09:07:56 +0200

    arm/arm64: pci: Add pci-testdev PCI device operation test
    Suggested-by: Andrew Jones <drjones@redhat.com>
    Cc: Thomas Huth <thuth@redhat.com>
    Cc: Andrew Jones <drjones@redhat.com>
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>

--- arm/pci-test.c
+++ arm/pci-test.c
@@ -8,12 +8,20 @@
 #include <libcflat.h>
 #include <pci.h>
 
+#define NR_TESTS (PCI_TESTDEV_NUM_BARS * PCI_TESTDEV_NUM_TESTS)
+
 int main(void)
 {
+	int ret;
+
 	if (!pci_probe())
 		report_abort("PCI bus probing failed\n");
 
 	pci_print();
+
+	ret = pci_testdev();
+	report("PCI test device passed %d/%d tests",
+		ret >= NR_TESTS, ret > 0 ? ret : 0, NR_TESTS);
 
 	return report_summary();
 }
